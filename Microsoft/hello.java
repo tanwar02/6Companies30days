a9d9d855e44e09677364b861293bf51b1a1cdd74
Sagar Tanwar <73141224+tanwar02@users.noreply.github.com>
2022-12-31 23:01:25 +0530

    Add files via upload

---
+++ Microsoft/hello.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class hello{
+
+    public static void main(String[] args) throws IOException{
+
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       System.out.println("hi");
+       br.close();
+    }
+}
